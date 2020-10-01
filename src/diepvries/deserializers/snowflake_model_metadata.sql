598c5cd0b8a291d0a722214e800e5711123d5b28
Dinis Louseiro <dimplouseiro@gmail.com>
2020-10-01 15:40:44 +0200

    Change picnic-data-vault to access metadata table
    ## Issue:
    As the metadata in our Snowflake database is getting bigger and bigger, we started experiencing a known issue from the past, when the queries to `information_schema` randomly fail with `Information schema query returned too much data.  Please repeat query with more selective predicates.`
    Even though the error message refers to the limit as being based on the number of rows returned by the query on `information_schema`, during some past chat with the Snowflake support, we were told that this limit is also based on other variables like the number of objects in the database, the number of queries running simultaneous, etc.
    
    ## Solution:
    Similarly to what was done in Pentaho, the solution is to access our metadata table (refreshed on each DV deployment) instead of the live query on `information_schema`.
    
    ## Implementation:
    Changed `src/picnic/data_vault/deserializers/snowflake_model_metadata.sql` to access `cfg.dv_metadata` instead of `information_schema.columns` directly.
    
    ## Notes:
    - This should be a temporary solution, as the correct solution is to use `SHOW COLUMNS IN SCHEMA` instead of querying the information schema directly. `SHOW` commands have no limitation in terms of number of rows, and are compatible with all snowflake databases. 
    - I did not change the code to use `SHOW` commands in this PR as we need a quick solution to ensure nobody wakes up during the night! Changing to the `SHOW` commands may seem simple, but the data returned by this command, comparatively to our current query is quite different and would demand more development time.

--- src/diepvries/deserializers/snowflake_model_metadata.sql
+++ src/diepvries/deserializers/snowflake_model_metadata.sql
@@ -1,13 +1,13 @@
 /*Query that fetches all needed properties to initialize a StagingTable and all DataVaultTable objects*/
 SELECT
-  LOWER(columns.table_name)                   AS "parent_table_name",
-  LOWER(columns.column_name)                  AS "name",
-  UPPER(columns.data_type)                    AS "data_type",
-  columns.ordinal_position::INTEGER           AS "position",
-  NOT (TO_BOOLEAN(columns.is_nullable))       AS "is_mandatory",
-  columns.numeric_precision::INTEGER          AS "precision",
-  columns.numeric_scale::INTEGER              AS "scale",
-  columns.character_maximum_length::INTEGER   AS "length"
-FROM information_schema.columns AS columns
-WHERE columns.table_schema = UPPER('{target_schema}')
-  AND LOWER(table_name) IN ({table_list});
+  LOWER(dm."tab_name")          AS "parent_table_name",
+  LOWER(dm."col_name")          AS "name",
+  UPPER(dm."col_datatype")      AS "data_type",
+  dm."col_position"             AS "position",
+  dm."col_not_null"             AS "is_mandatory",
+  dm."col_precision"            AS "precision",
+  dm."col_scale"                AS "scale",
+  NULLIF(dm."col_length", '-1') AS "length"
+FROM cfg.dv_metadata AS dm
+WHERE dm."sch_name" = LOWER('{target_schema}')
+  AND dm."tab_name" IN ({table_list});
