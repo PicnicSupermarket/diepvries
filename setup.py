2bc68ddd6adb46a6d84f5212fc206c2c4f53137d
Matthieu Caneill <matthieu.caneill@teampicnic.com>
2021-06-15 14:44:31 +0200

    Add bash script to build and upload Python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""This is provided here for the sole purpose of editable installations.
+
+See https://twitter.com/pganssle/status/1241161328137515008
+"""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
