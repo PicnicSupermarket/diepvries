bc1360738ba05253ceac3f5a7092abd0251f5265
Dinis Louseiro <dimplouseiro@gmail.com>
2021-08-09 10:55:16 +0200

    Unify SQL hub/link DML statements

--- src/diepvries/template_sql/hub_dml.sql
+++
@@ -1,9 +0,0 @@
-MERGE INTO {target_schema}.{target_table} AS hub
-    USING (
-        SELECT {hashkey_field},
-               {non_hashkey_fields_aggregation}
-        FROM {staging_schema}.{staging_table}
-        GROUP BY {hashkey_field}
-    ) AS staging ON ({business_key_condition})
-    WHEN NOT MATCHED THEN INSERT ({hashkey_field}, {non_hashkey_fields})
-        VALUES (staging.{hashkey_field}, {staging_non_hashkey_fields});
