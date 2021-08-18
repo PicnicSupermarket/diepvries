2558f304657eadc029d57a9becfa224f51f378f3
Dinis Louseiro <dimplouseiro@gmail.com>
2021-08-18 14:58:11 +0200

    Revert "Adapt test suite to new MERGE statement"
    This reverts commit 6aff06c939088becd2a7847d577ba25ecad04415.

--- test/sql/expected_result_hub.sql
+++ test/sql/expected_result_hub.sql
@@ -1,8 +1,10 @@
 MERGE INTO dv.h_customer AS target
   USING (
-        SELECT DISTINCT
-          h_customer_hashkey,r_timestamp,r_source,customer_id
+        SELECT
+          h_customer_hashkey,
+          MIN(r_timestamp) AS r_timestamp,MIN(r_source) AS r_source,MIN(customer_id) AS customer_id
         FROM dv_stg.orders_20190806_000000
+        GROUP BY h_customer_hashkey
         ) AS staging ON (target.h_customer_hashkey = staging.h_customer_hashkey)
-  WHEN NOT MATCHED THEN INSERT (h_customer_hashkey,r_timestamp,r_source,customer_id)
-    VALUES (staging.h_customer_hashkey,staging.r_timestamp,staging.r_source,staging.customer_id);
+  WHEN NOT MATCHED THEN INSERT (h_customer_hashkey, r_timestamp,r_source,customer_id)
+    VALUES (staging.h_customer_hashkey, staging.r_timestamp,staging.r_source,staging.customer_id);
--- test/sql/expected_result_role_playing_hub.sql
+++ test/sql/expected_result_role_playing_hub.sql
@@ -1,8 +1,10 @@
 MERGE INTO dv.h_customer AS target
   USING (
-        SELECT DISTINCT
-          h_customer_role_playing_hashkey,r_timestamp,r_source,customer_role_playing_id
+        SELECT
+          h_customer_role_playing_hashkey,
+          MIN(r_timestamp) AS r_timestamp,MIN(r_source) AS r_source,MIN(customer_role_playing_id) AS customer_role_playing_id
         FROM dv_stg.orders_20190806_000000
+        GROUP BY h_customer_role_playing_hashkey
         ) AS staging ON (target.h_customer_hashkey = staging.h_customer_role_playing_hashkey)
-  WHEN NOT MATCHED THEN INSERT (h_customer_hashkey,r_timestamp,r_source,customer_id)
-    VALUES (staging.h_customer_role_playing_hashkey,staging.r_timestamp,staging.r_source,staging.customer_role_playing_id);
+  WHEN NOT MATCHED THEN INSERT (h_customer_hashkey, r_timestamp,r_source,customer_id)
+    VALUES (staging.h_customer_role_playing_hashkey, staging.r_timestamp,staging.r_source,staging.customer_role_playing_id);
