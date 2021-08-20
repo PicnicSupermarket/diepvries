106a0757f430d32180ea59294caaa7a734e64cbb
Dinis Louseiro <dimplouseiro@gmail.com>
2021-08-20 12:12:49 +0200

    Adapt test suite to new queries

--- test/sql/expected_result_link.sql
+++ test/sql/expected_result_link.sql
@@ -1,7 +1,13 @@
 MERGE INTO dv.l_order_customer AS target
   USING (
         SELECT DISTINCT
-          l_order_customer_hashkey, h_order_hashkey, h_customer_hashkey, order_id, customer_id, ck_test_string, ck_test_timestamp, r_timestamp, r_source
+          l_order_customer_hashkey,
+          -- If multiple sources for the same hashkey are received, their values
+          -- are concatenated using a comma.
+          LISTAGG(DISTINCT r_source, ',')
+              WITHIN GROUP (ORDER BY r_source)
+              OVER (PARTITION BY l_order_customer_hashkey) AS r_source,
+          h_order_hashkey, h_customer_hashkey, order_id, customer_id, ck_test_string, ck_test_timestamp, r_timestamp
         FROM dv_stg.orders_20190806_000000
         ) AS staging ON (target.l_order_customer_hashkey = staging.l_order_customer_hashkey)
   WHEN NOT MATCHED THEN INSERT (l_order_customer_hashkey, h_order_hashkey, h_customer_hashkey, order_id, customer_id, ck_test_string, ck_test_timestamp, r_timestamp, r_source)
