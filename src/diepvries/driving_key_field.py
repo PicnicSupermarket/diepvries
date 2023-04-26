cff08719325409695a65771f90d4a640ca0d445d
lianogueira <lia.lnm@gmail.com>
2023-04-26 09:32:18 +0200

    Add validation for driving keys

--- src/diepvries/driving_key_field.py
+++ src/diepvries/driving_key_field.py
@@ -2,6 +2,8 @@
 
 from dataclasses import dataclass
 
+from . import TABLE_PREFIXES, TableType
+
 
 @dataclass
 class DrivingKeyField:
@@ -12,12 +14,20 @@ class DrivingKeyField:
     defined by a subset of hashkeys.
     """
 
-    #: Name of parent table in the database (always a Link).
-    parent_table_name: str
-    #: Column name in the database.
-    name: str
-    #: Name of the satellite where this field represents a driving key.
-    satellite_name: str
+    def __init__(self, parent_table_name: str, name: str, satellite_name: str):
+        """Instantiate a DrivingKeyField.
+
+        Args:
+            parent_table_name: Name of parent table in the database (always a Link).
+            name: Column name in the database.
+            satellite_name: Name of the satellite where this field
+                represents a driving key.
+        """
+        self.parent_table_name = parent_table_name
+        self.name = name
+        self.satellite_name = satellite_name
+
+        self._validate()
 
     def __str__(self) -> str:
         """Representation of a DrivingKeyField object as a string.
@@ -31,3 +41,34 @@ class DrivingKeyField:
             f"{type(self).__name__}: field_name={self.name}, "
             f"satellite={self.satellite_name}"
         )
+
+    def _validate(self):
+        """Validate driving key
+
+        Perform the following checks:
+        1. Satellite name has a prefix within TABLE_PREFIXES[TableType.SATELLITE]
+        2. Parent table name has a prefix within TABLE_PREFIXES[TableType.LINK]
+
+        Raises:
+            RuntimeError: If one of the checks fails.
+        """
+
+        satellite_name_prefix = next(
+            split_part for split_part in self.satellite_name.split("_")
+        )
+        if satellite_name_prefix not in TABLE_PREFIXES[TableType.SATELLITE]:
+            raise RuntimeError(
+                f"'{self.satellite_name}': Satellite name with incorrect prefix. "
+                f"Got '{satellite_name_prefix}', "
+                f"but expects '{TABLE_PREFIXES[TableType.SATELLITE]}'."
+            )
+
+        parent_table_name_prefix = next(
+            split_part for split_part in self.parent_table_name.split("_")
+        )
+        if parent_table_name_prefix not in TABLE_PREFIXES[TableType.LINK]:
+            raise RuntimeError(
+                f"'{self.satellite_name}': Parent table with incorrect prefix. "
+                f"Got '{parent_table_name_prefix}', "
+                f"but expects '{TABLE_PREFIXES[TableType.LINK]}'."
+            )
