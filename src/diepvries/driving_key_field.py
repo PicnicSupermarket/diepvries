19f2119ca62fdc12eb26d8ce679fc021575c8eba
Lia Nogueira <lianogueira@users.noreply.github.com>
2023-05-22 11:35:15 +0200

    Add validation for driving keys (#41)
    **Issue**: We noticed an issue with duplicates in the link/eff_sattelite tables when the **incorrect** prefix is given to the table names. 
    
    **Solution**: Add a validation that will fail the process when:
    * The prefix of the `satellite_name` is not within `TABLE_PREFIXES[TableType.SATELLITE]`.
    * The prefix of the `parent_table_name` is not within `TABLE_PREFIXES[TableType.LINK]`.
    
    **Implementation**: Add a validation method in the instantiation of a driving key.

--- src/diepvries/driving_key_field.py
+++ src/diepvries/driving_key_field.py
@@ -2,6 +2,8 @@
 
 from dataclasses import dataclass
 
+from . import TABLE_PREFIXES, TableType
+
 
 @dataclass
 class DrivingKeyField:
@@ -19,6 +21,10 @@ class DrivingKeyField:
     #: Name of the satellite where this field represents a driving key.
     satellite_name: str
 
+    def __post_init__(self):
+        """Validate the driving key."""
+        self._validate()
+
     def __str__(self) -> str:
         """Representation of a DrivingKeyField object as a string.
 
@@ -30,4 +36,32 @@ class DrivingKeyField:
         return (
             f"{type(self).__name__}: field_name={self.name}, "
             f"satellite={self.satellite_name}"
+        )
+
+    def _validate(self):
+        """Validate driving key.
+
+        Perform the following checks:
+        1. Satellite name has a prefix within TABLE_PREFIXES[TableType.SATELLITE].
+        2. Parent table name has a prefix within TABLE_PREFIXES[TableType.LINK].
+
+        Raises:
+            AssertionError: If one of the checks fails.
+        """
+        satellite_name_prefix = next(
+            split_part for split_part in self.satellite_name.split("_")
+        )
+        assert satellite_name_prefix in TABLE_PREFIXES[TableType.SATELLITE], (
+            f"'{self.satellite_name}': Satellite name with incorrect prefix. "
+            f"Got '{satellite_name_prefix}', "
+            f"but expects '{TABLE_PREFIXES[TableType.SATELLITE]}'."
+        )
+
+        parent_table_name_prefix = next(
+            split_part for split_part in self.parent_table_name.split("_")
+        )
+        assert parent_table_name_prefix in TABLE_PREFIXES[TableType.LINK], (
+            f"'{self.satellite_name}': Parent table with incorrect prefix. "
+            f"Got '{parent_table_name_prefix}', "
+            f"but expects '{TABLE_PREFIXES[TableType.LINK]}'."
         )
---
+++ test/test_driving_key_field.py
@@ -0,0 +1,38 @@
+"""Unit test for DrivingKeyField."""
+import pytest
+from diepvries.driving_key_field import DrivingKeyField
+
+
+def test_valid_driving_key():
+    """Check that no errors are raised when instantiating a
+    DrivingKeyField with valid values.
+    """
+    DrivingKeyField(
+        name="h_customer_hashkey",
+        parent_table_name="l_order_customer",
+        satellite_name="ls_order_customer_eff",
+    )
+
+
+def test_invalid_satellite_name():
+    """Check that an `AssertionError` is raised when an invalid
+    satellite_name is provided.
+    """
+    with pytest.raises(AssertionError):
+        DrivingKeyField(
+            name="h_customer_hashkey",
+            parent_table_name="l_order_customer",
+            satellite_name="invalid_satellite_name",
+        )
+
+
+def test_invalid_parent_table_name():
+    """Check that an `AssertionError` is raised when an invalid
+    parent_table_name is provided.
+    """
+    with pytest.raises(AssertionError):
+        DrivingKeyField(
+            name="h_customer_hashkey",
+            parent_table_name="invalid_parent_table_name",
+            satellite_name="ls_order_customer_eff",
+        )
